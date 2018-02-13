97d05c834a85508425d007147e01d98c0971167b
Adam Hansen <adamlars90@gmail.com>
2018-02-13 09:12:25 +0100

    Added CrimeFragment
    - Created CrimeFragment
    - Added CrimeFragment to CrimeActivity
    - Setup strings for application

--- app/src/main/java/dk/adamino/criminalintent/CrimeActivity.java
+++ app/src/main/java/dk/adamino/criminalintent/CrimeActivity.java
@@ -1,7 +1,10 @@
 package dk.adamino.criminalintent;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class CrimeActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
