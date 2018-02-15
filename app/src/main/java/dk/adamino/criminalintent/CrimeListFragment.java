6d98fc41ecacd1d278e0762b6db8cc1a74cce782
Adam Hansen <adamlars90@gmail.com>
2018-02-15 10:01:15 +0100

    Added swiping
    - Refactored CrimeActivity to CrimePagerActivity for swiping between
    crimes!

--- app/src/main/java/dk/adamino/criminalintent/CrimeListFragment.java
+++ app/src/main/java/dk/adamino/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package dk.adamino.criminalintent;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -28,7 +29,7 @@ public class CrimeListFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
 
         mCrimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
@@ -86,7 +87,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
     }
